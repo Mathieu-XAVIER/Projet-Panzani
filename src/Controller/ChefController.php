814c0cd05bc307d4cdee7f1c1e4d6c4598ce26b9
Mathieu <mathieuxavier38150@gmail.com>
2023-04-28 09:27:11 +0200

    added form

--- src/Controller/ChefController.php
+++ src/Controller/ChefController.php
@@ -3,20 +3,41 @@
 namespace App\Controller;
 
 use App\Entity\Chef;
+use App\Entity\Client;
+use App\Form\Client1Type;
+use App\Repository\ClientRepository;
 use Doctrine\Persistence\ManagerRegistry;
+
+use PhpParser\Node\Expr\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChefController extends AbstractController
 {
     #[Route('/', name: 'app_chef')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, Request $request, ClientRepository $clientRepository): Response
     {
         $repo_chefs = $doctrine->getRepository(Chef::class);
         $chefs = $repo_chefs->findAll();
+
+        $client = new Client();
+        $form = $this->createForm(Client1Type::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $clientRepository->save($client, true);
+
+            return $this->redirectToRoute('app_chef', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('chef/index.html.twig', [
             'chefs' => $chefs,
+            'form' => $form->createView(),
         ]);
+
+
+
     }
 }
